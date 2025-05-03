c4a7b875aa519be4df238ce9a58ebfe1d1a29134
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-03 21:27:20 +0900

    box_garb: revise
    remove extern

--- source/box_garb.h
+++ source/box_garb.h
@@ -1,9 +1,9 @@
 #ifndef BOX_GARB_H
 #define BOX_GARB_H
 
-extern void create_new_boxlog(short unr, bool bullids);
-extern void delete_tempboxfiles(void);
-extern void garbage_collection(bool xgar, bool fill_cbyte, bool check_all,
+void create_new_boxlog(short unr, bool bullids);
+void delete_tempboxfiles(void);
+void garbage_collection(bool xgar, bool fill_cbyte, bool check_all,
 			       bool immediate, short unr);
 
 #endif
