e71616d6f8b603f9cb5043e793d90608fbd74dba
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-04 17:19:11 +0900

    box_sub: revise
    remove extern

--- source/box_sub.h
+++ source/box_sub.h
@@ -7,106 +7,106 @@
 #include "yapp.h"
 #include "boxlocal.h"
 
-extern void send_final_unproto();
-extern void send_new_unproto_header(boxlogstruct header);
-extern void send_requested_unproto(unprotoportstype *hp);
-extern void unproto_request1(char *call, char *port, char *path, char *s);
-extern void clear_msgnumarr(void);
-extern void fill_msgnumarr(void);
-extern short msgnum2board(int32_t msgnum, char *board);
-extern int32_t msgnum2centry(int32_t msgnum, boxlogstruct *log, bool fuzzy);
-extern short actual_connects(void);
-extern bool wildcardcompare(short fangraster, char *w, char *t, char *wr);
-extern int32_t cpu_usage(short unr);
-extern void expand_macro(short unr, char *hs);
-extern bool in_rsysops_boards(char *call, char *board);
-extern void trace_cmd(short unr, char *w, char *w1);
-extern short in_tracecalls(char *call);
-extern void delete_trace_from(short unr);
-extern void delete_all_tracecalls(void);
-extern bool in_protocalls(char *call);
-extern void delete_all_protocalls(void);
-extern void reload_proto(void);
-extern void proto_cmd(short unr, char *w, char *w1);
-extern void get_boxruntime_s(short what, char *hs);
-extern time_t get_boxruntime_l(void);
-extern void reset_boxruntime(void);
-extern void flush_bidseek(void);
-extern void flush_bidseek_immediately(void);
-extern void update_bidseek(void);
-extern bool load_bidseek(void);
-extern void create_hcs(indexstruct *h);
-extern bool check_hcs(indexstruct h);
-extern cutboxtyp boxheader(char *zeile);
-extern bool boxrange(short unr);
-extern void upd_statistik(short unr, int32_t txbytes, int32_t rxbytes, int64_t start,
+void send_final_unproto();
+void send_new_unproto_header(boxlogstruct header);
+void send_requested_unproto(unprotoportstype *hp);
+void unproto_request1(char *call, char *port, char *path, char *s);
+void clear_msgnumarr(void);
+void fill_msgnumarr(void);
+short msgnum2board(int32_t msgnum, char *board);
+int32_t msgnum2centry(int32_t msgnum, boxlogstruct *log, bool fuzzy);
+short actual_connects(void);
+bool wildcardcompare(short fangraster, char *w, char *t, char *wr);
+int32_t cpu_usage(short unr);
+void expand_macro(short unr, char *hs);
+bool in_rsysops_boards(char *call, char *board);
+void trace_cmd(short unr, char *w, char *w1);
+short in_tracecalls(char *call);
+void delete_trace_from(short unr);
+void delete_all_tracecalls(void);
+bool in_protocalls(char *call);
+void delete_all_protocalls(void);
+void reload_proto(void);
+void proto_cmd(short unr, char *w, char *w1);
+void get_boxruntime_s(short what, char *hs);
+time_t get_boxruntime_l(void);
+void reset_boxruntime(void);
+void flush_bidseek(void);
+void flush_bidseek_immediately(void);
+void update_bidseek(void);
+bool load_bidseek(void);
+void create_hcs(indexstruct *h);
+bool check_hcs(indexstruct h);
+cutboxtyp boxheader(char *zeile);
+bool boxrange(short unr);
+void upd_statistik(short unr, int32_t txbytes, int32_t rxbytes, int64_t start,
 			  int64_t stop);
-extern void get_btext(short unr, short nr, char *s);
-extern void x_w_btext(short unr, short nr, bool lf);
-extern void no_files(short unr, char *name);
-extern short find_buser(short tnr, short pchan);
-extern short actual_user(char *calls);
-extern void show_rfile(short unr, char *base, int32_t size, bool mit_r,
+void get_btext(short unr, short nr, char *s);
+void x_w_btext(short unr, short nr, bool lf);
+void no_files(short unr, char *name);
+short find_buser(short tnr, short pchan);
+short actual_user(char *calls);
+void show_rfile(short unr, char *base, int32_t size, bool mit_r,
 		       bool sf, bool only_headers);
-extern void show_textfile(short unr, char *name);
-extern void show_help(short unr, char *w);
-extern bool defined_board(char *rubr);
-extern void switch_to_default_board(char *rubr);
-extern void switch_to_default_fserv_board(char *rubr);
-extern void check_verteiler(char *rubr);
-extern void check_transfers(char *rubr);
-extern void extend_6_to_8(char *rubr);
-extern bool valid_boardname(char *rubrik);
-extern bool strip_invalid_boardname_chars(char *rubrik);
-extern void chk_maxlt(unsigned short *lt1, unsigned short ltd);
-extern void check_lt_acc(char *rubr, unsigned short *lt, unsigned short *acc);
-extern bool may_sysaccess(short unr, char *board);
-extern void check_msgtype(char *msgtype, char *ziel, char *betreff);
-extern int32_t truesize(indexstruct rec);
-extern char separate_status(char *status, char *ziel, char *absender,
+void show_textfile(short unr, char *name);
+void show_help(short unr, char *w);
+bool defined_board(char *rubr);
+void switch_to_default_board(char *rubr);
+void switch_to_default_fserv_board(char *rubr);
+void check_verteiler(char *rubr);
+void check_transfers(char *rubr);
+void extend_6_to_8(char *rubr);
+bool valid_boardname(char *rubrik);
+bool strip_invalid_boardname_chars(char *rubrik);
+void chk_maxlt(unsigned short *lt1, unsigned short ltd);
+void check_lt_acc(char *rubr, unsigned short *lt, unsigned short *acc);
+bool may_sysaccess(short unr, char *board);
+void check_msgtype(char *msgtype, char *ziel, char *betreff);
+int32_t truesize(indexstruct rec);
+char separate_status(char *status, char *ziel, char *absender,
 			    char *mbx, char *datum, char *zeit, char *laenge,
 			    char *lifetime);
-extern void create_status(int32_t additional, bool hierarchicals, char *name,
+void create_status(int32_t additional, bool hierarchicals, char *name,
 			  indexstruct header, char *status);
-extern void create_status2(bool hierarchicals, char *dest, char *absender,
+void create_status2(bool hierarchicals, char *dest, char *absender,
 			   int32_t rxdate, int32_t expire_time, int32_t size,
 			   char msgtype, char *status);
-extern void create_userid(char *call, char *absender, char *datum, char *zeit,
+void create_userid(char *call, char *absender, char *datum, char *zeit,
 			  char *betreff, char *userid);
-extern void new_bid(char *bid);
-extern int32_t new_msgnum(void);
-extern int32_t messagerxtime(void);
-extern void split_sline(char *eingabe, char *call1, char *call2, char *mbx,
+void new_bid(char *bid);
+int32_t new_msgnum(void);
+int32_t messagerxtime(void);
+void split_sline(char *eingabe, char *call1, char *call2, char *mbx,
 			char *bid, char *lt, char *betreff);
-extern bool check_for_dirty(char *hs);
-extern bool check_for_dirty2(char *bs, char *hs);
-extern void check_reject(char *frombox, char msgchar, char *ziel, char *mbx,
+bool check_for_dirty(char *hs);
+bool check_for_dirty2(char *bs, char *hs);
+void check_reject(char *frombox, char msgchar, char *ziel, char *mbx,
   char *absender, char *lifetime, char *betreff, char *bid, int32_t laenge,
   bool is_local, bool is_privlocal, bool *reject_it, bool *no_sf,
   char *rejectreason);
-extern void do_convtit(char *frombox, char msgchar, char *ziel, char *mbx,
+void do_convtit(char *frombox, char msgchar, char *ziel, char *mbx,
   char *absender1, char *lifetime1, char *betreff1, char *bid, int32_t laenge,
   bool is_local, bool is_privlocal, bool *reject_it, bool *no_sf);
-extern void create_ack(char *absender, char *ackcall, char *dest,
+void create_ack(char *absender, char *ackcall, char *dest,
 		       char *umleitung, char *betreff);
-extern void show_prompt(short unr);
-extern void check_replytitle(char *reply);
-extern void get_numbers(short unr, bool plong, char *eingabe, int32_t *start,
+void show_prompt(short unr);
+void check_replytitle(char *reply);
+void get_numbers(short unr, bool plong, char *eingabe, int32_t *start,
 			int32_t *ende, short *threshold, char *option, char *search);
-extern short find_command(short unr, char *kommando, bool *onlysys);
-extern void expand_command(short unr, char *kommando);
-extern short str2disturb_chan(char *w);
-extern short str2disturb_chan2(char *w);
-extern void fill_logline(bool full, short unr, char *hs);
-extern void box_logbuch(short unr);
-extern void send_conv(short unr, char *s);
-extern void box_convers(short unr, char *cmd);
-extern bool use_gzip(int32_t size);
-extern void sane_shutdown(short magic);
-extern bool was_sane_shutdown(void);
-extern void set_lastgarbagetime(time_t garbagetime);
-extern time_t get_lastgarbagetime(void);
-extern time_t get_totalruntime_l(void);
+short find_command(short unr, char *kommando, bool *onlysys);
+void expand_command(short unr, char *kommando);
+short str2disturb_chan(char *w);
+short str2disturb_chan2(char *w);
+void fill_logline(bool full, short unr, char *hs);
+void box_logbuch(short unr);
+void send_conv(short unr, char *s);
+void box_convers(short unr, char *cmd);
+bool use_gzip(int32_t size);
+void sane_shutdown(short magic);
+bool was_sane_shutdown(void);
+void set_lastgarbagetime(time_t garbagetime);
+time_t get_lastgarbagetime(void);
+time_t get_totalruntime_l(void);
 
 #define w_btext(unr, nr) x_w_btext(unr, nr, false)
 #define wln_btext(unr, nr) x_w_btext(unr, nr, true)
