840cc3ccb6250505a0b85109872e83cb988fcf14
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-04 16:01:01 +0900

    box_scan: revise
    remove extern

--- source/box_scan.h
+++ source/box_scan.h
@@ -4,13 +4,13 @@
 
 #include "boxlocal.h"
 
-extern void check_accepted_bad_rcalls_syntax(char *s);
-extern void create_my_rline(time_t rxdate, char *bid, char *rline);
-extern char *get_rtoken(char *rline, char *token, char *para, short maxlen);
-extern char *get_w0rli_call(char *rline, char *para);
-extern void get_rcall(char *rline, char *rcall);
-extern time_t get_headerdate(char *timestr);
-extern bool scan_for_ack(char *puffer, int32_t size, bool wpupdate,
+void check_accepted_bad_rcalls_syntax(char *s);
+void create_my_rline(time_t rxdate, char *bid, char *rline);
+char *get_rtoken(char *rline, char *token, char *para, short maxlen);
+char *get_w0rli_call(char *rline, char *para);
+void get_rcall(char *rline, char *rcall);
+time_t get_headerdate(char *timestr);
+bool scan_for_ack(char *puffer, int32_t size, bool wpupdate,
 		     bool wprotupdate, bool part,
                      char *absender, char *board, char *subject, char *mbx,
 		     char *bid, char msgtype, char *ackcall, bool *is_binary,
