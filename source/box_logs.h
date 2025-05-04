0e8b71fefc8f52d4e278d7084f1969c10613708e
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-04 13:30:09 +0900

    box_logs: revise
    remove extern

--- source/box_logs.h
+++ source/box_logs.h
@@ -1,18 +1,18 @@
 #ifndef BOX_LOGS_H
 #define BOX_LOGS_H
 
-extern void append_rejectlog(short unr, char mtype, char *sender, char *board, char *mbx, char *bid,
+void append_rejectlog(short unr, char mtype, char *sender, char *board, char *mbx, char *bid,
       	      	      	     char *size, char *comment);
-extern void append_profile(short unr, char *txt);
-extern void append_readlog(short unr, char *txt);
-extern void append_syslog(short unr);
-extern void append_userlog(short unr);
-extern void append_usersflog(short unr, char *sender, char *board, char *mbx, char *bid, char *size);
-extern void append_protolog(short unr);
-extern void append_sflog(short unr);
-extern void append_convlog(short unr, char *txt);
-extern bool get_debug_func(short pn, char *p);
-extern void debug_2(short level, short unr, short pn, char *txt);
+void append_profile(short unr, char *txt);
+void append_readlog(short unr, char *txt);
+void append_syslog(short unr);
+void append_userlog(short unr);
+void append_usersflog(short unr, char *sender, char *board, char *mbx, char *bid, char *size);
+void append_protolog(short unr);
+void append_sflog(short unr);
+void append_convlog(short unr, char *txt);
+bool get_debug_func(short pn, char *p);
+void debug_2(short level, short unr, short pn, char *txt);
 
 #define debug(level, unr, pn, txt)	if (level <= debug_level) debug_2(level, unr, pn, txt); \
 					else lastproc = pn
