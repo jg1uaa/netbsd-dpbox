77d31b24a581884598ea2075f4a7bb8608c83c10
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-04 17:24:29 +0900

    boxdbox: revise
    remove extern

--- source/boxdbox.h
+++ source/boxdbox.h
@@ -5,8 +5,8 @@
 #include "pastrix.h"
 #include "boxglobl.h"
 
-extern void cdbtboard(char *s);
+void cdbtboard(char *s);
 /* hier holt sich new_entry() den transferierten brettnamen her */
-extern void conv_diebox(short unr, char *eingabe);
+void conv_diebox(short unr, char *eingabe);
 
 #endif
